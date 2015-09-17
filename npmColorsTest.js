b7ea7d02a27f5f9e6c8634316a68ed2b1fab282e
Claus Bové <clbo@kea.dk>
2015-09-17 07:07:46 +0200

    initial commit

---
+++ npmColorsTest.js
@@ -0,0 +1,5 @@
+var colors = require('colors');
+
+console.log('This is som yellow text'.yellow);
+console.log('This is som yellow and bold text'.yellow.bold);
+console.log('This is som yellow and bold text'.yellow.bold.underline);
